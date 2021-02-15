d92abe252739d851aeb951ebf2200fe0e2151e6f
Wiesław <61698374+WieslawC@users.noreply.github.com>
2021-02-15 09:05:53 +0100

    Updated media queries/ optymized js code

--- code.js
+++ code.js
@@ -1,66 +1,41 @@
-//textTyping
-const aboutText = "About me";
+/* #region  TYPE TEXT FUNCTION */
+const typeText = function (inputText, targetContainer) {
+  let textToDisplay = "";
+  let currentLetterPossition = 0;
 
-let textToDisplay = '';
-let i = 0;
-const target = $('div.about p.aboutTitle');
-
-let itsTimeToStop = setInterval(()=>{
-    textToDisplay += aboutText[i++];
-    target.text(`${textToDisplay}|`);
-    if(i == aboutText.length){
-        clearInterval(itsTimeToStop);
-        target.text(textToDisplay);
-    } 
-},100);
-
-
-//parallax background scroll
-$(document).scroll(()=>{
-    $("header img.headerBackgroundImage").css("top", `${$(window).scrollTop()/2}px`);
-})
-
-//show menu icon
-if(window.innerWidth > 750)
-{
-    $(document).scroll(()=>{
-        if($(window).scrollTop() > 100){
-            document.querySelector('.headerNavigation').classList.add('active');
-        }
-        else {
-            document.querySelector('.headerNavigation').classList.remove('active');
-        }
-    })
-}
-else{
-    document.querySelector('.headerNavigation').classList.remove('active');
-}
-
-
-$('.menuIcon').click(()=>{
-    document.querySelector('.headerNavigation').classList.toggle('active');
-})
-
-
-$(".menuHome").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("body").offset().top 
-    }, 1000);
-    if($('.headerNavigation').hasClass('active')){
-        document.querySelector('.headerNavigation').classList.toggle('active');
+  let itsTimeToStop = setInterval(() => {
+    textToDisplay += inputText[currentLetterPossition++];
+    targetContainer.text(`${textToDisplay}|`);
+    if (currentLetterPossition == inputText.length) {
+      clearInterval(itsTimeToStop);
+      targetContainer.text(textToDisplay);
     }
+  }, 100);
+}
+/* #endregion */
+
+typeText("About me", $("div.about p.aboutTitle"));
+
+//PARALLAX BACKGROUND SCROLL
+$(document).scroll(() => {
+  $("header img.headerBackgroundImage").css("top", `${$(window).scrollTop() / 2}px`);
 });
 
-$(".menuAbout").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $(".about").offset().top - 100
-    }, 1000);
-    document.querySelector('.headerNavigation').classList.toggle('active');
-});
+//TOGGLE MOBILE MENU
+$('.menuIcon').click(() => {
+  $('.headerNavigation').toggleClass('active');
+})
 
-$(".menuProjects").click(function() {
+
+const scrollToElement = function (trigger, target, time = 1000) {
+  trigger.click(() => {
     $([document.documentElement, document.body]).animate({
-        scrollTop: $(".projects").offset().top 
-    }, 1000);
-    document.querySelector('.headerNavigation').classList.toggle('active');
-});
+      scrollTop: target.offset().top
+    }, time);
+    $('.headerNavigation').toggleClass('active');
+  })
+}
+
+scrollToElement($('.menuHome'), $('.wrapper'));
+scrollToElement($('.menuAbout'), $('div.about'))
+scrollToElement($('.menuProjects'), $('div.projects'))
